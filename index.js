66b4348e1dac4245c31898a3bd201322c3549a34
faizal8089 <faizalsulthan288@gmail.com>
2021-12-17 21:46:25 +0530

    commit

--- index.js
+++ index.js
@@ -18,7 +18,11 @@ function display(){
     let listItems = "";
     for(let i = 0; i < myLeads.length; i++){
         // listItems += "<li><a href='#' target='_blank'>" + myLeads[i] + "</a></li>";
-        listItems += `<li><a href='#' target='_blank'>" + myLeads[i] + "</a></li>`;
+        listItems += `<li>
+                        <a href='${myLeads[i]}' target='_blank'>
+                        ${myLeads[i]}
+                        </a>
+                    </li>`;
     }
     ulEl.innerHTML = listItems;
 }

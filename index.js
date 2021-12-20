4d2aad024586c38cde3f3e1970d33b9f1607ece8
faizal8089 <faizalsulthan288@gmail.com>
2021-12-20 20:58:16 +0530

    commit

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
 
 let myLeads = [];
 
+
 const input = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
 const inputBtn = document.getElementById("input-btn");
-const dis = document.getElementById("dis");
+const reset = document.getElementById("reset");
+
+if(localStorage.getItem("myLeads")!=null){
+    display();
+}
+
+reset.addEventListener("dblclick", function(){
+
+    localStorage.clear();
+    myLeads = [];
+    ulEl.innerHTML = null;
+    
+});
 
 inputBtn.addEventListener("click", function (){
-    myLeads.push(input.value);
-    display();
-    input.value = "";
+    if(input.value != ""){
+        myLeads.push(input.value);
+        input.value = "";
+        myLeads = JSON.stringify(myLeads);
+        localStorage.setItem("myLeads", myLeads);
+        display();
+    }
 });
 
 
 
 function display(){
     let listItems = "";
+    myLeads = localStorage.getItem("myLeads");
+    myLeads = JSON.parse(myLeads);
     for(let i = 0; i < myLeads.length; i++){
-        // listItems += "<li><a href='#' target='_blank'>" + myLeads[i] + "</a></li>";
         listItems += `<li>
                         <a href='${myLeads[i]}' target='_blank'>
                         ${myLeads[i]}
@@ -25,6 +43,7 @@ function display(){
                     </li>`;
     }
     ulEl.innerHTML = listItems;
+
 }
 
 
